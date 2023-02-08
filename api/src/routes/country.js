fbc237fbed66331009a7a64a2bfaf8a410d6f08a
Santiago Bavaresco <santiago.bavaresco@codere.com>
2023-02-08 11:12:53 -0300

    API save in DB

--- api/src/routes/country.js
+++ api/src/routes/country.js
@@ -30,25 +30,58 @@ router.post("/BringCountriesFromApi", async (req, res)=>{
     } 
     
     catch (error) {
-        res.status(404).json(error.message)
-        
+        res.status(404).json(error.message)       
     }
         
 });
 
 
-router.get("/All", async (req , res)=>{
+router.get("/", async (req , res)=>{
     console.log("ESTE ES EL ALL")
     const allCountries = await Country.findAll()
-   
-     if(!allCountries){
-        res.status(400).send("No Countries")
-     }
-        
-     else {
-        res.status(200).send(allCountries)
+   try {
+        if(allCountries.length ===0){
+            
+            res.status(400).send("No Countries")
+        }
+        else {
+            res.status(200).json(allCountries)
+        }
+   } 
+   catch (error) {
+        res.status(400).send(error.message)
+   }
+})
+
+router.get("/", async (req , res)=>{ 
+
+
+});
+
+
+router.get("/:idPais", async (req , res)=>{ 
+    const { idPais } = req.params;
+    idPais.toUpperCase()
+    console.log(idPais.toUpperCase());
+    try {
+        const countryFound = await Country.findByPk(idPais.toUpperCase());
+
+        if(!countryFound) throw Error;
+            return res.status(200).json(countryFound);
+    } 
+    catch (error) {
+        return res.status(404)
+        .send(`El código ${idPais} no corresponde a un personaje existente`)
     }
     
-})
+});
+// [ ] GET /countries/{idPais}:
+// Obtener el detalle de un país en particular
+// Debe traer solo los datos pedidos en la ruta de detalle de país
+// Incluir los datos de las actividades turísticas correspondientes
+
+// [ ] GET /countries?name="...":
+// Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto)
+// Si no existe ningún país mostrar un mensaje adecuado
 
 module.exports = router;
