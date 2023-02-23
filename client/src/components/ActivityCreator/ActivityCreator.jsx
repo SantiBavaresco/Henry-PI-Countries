bcc58b132501e85f2e2f98e6a7169eb4facd9e6c
SantiBavaresco <103169408+SantiBavaresco@users.noreply.github.com>
2023-02-23 00:20:10 -0300

    Frontend 1.4.2

--- client/src/components/ActivityCreator/ActivityCreator.jsx
+++ client/src/components/ActivityCreator/ActivityCreator.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { connect, useDispatch, useSelector } from "react-redux";
+import { getAllCountries, getActivities, createActivity} from "../../redux/actions";
+
 import styles from "./ActivityCreator.module.css"
 import DualColumnScrollBarLabel from './DualColumnScrollBarLabel';
 import InputActivityCreator from "../InputActivityCreator/InputActivityCreator"
 
 
-function ActivityCreator() {
+function ActivityCreator(props) {
   //   const [selectedOption, setSelectedOption] = useState('Option 1');
   // const [selectedActivities, setSelectedActivities] = useState([]);
   // const [selectedCountries, setSelectedCountries] = useState([]);
@@ -44,24 +47,105 @@ function ActivityCreator() {
   //     setSelectedCountries(selectedCountries.filter((c) => c !== country));
   //   }
   // };
-  const [inputArray, setInputArray] = useState("");
-  const [dualColumnArray, setDualColumnArray] = useState([]);
+  const { allCountries, allActivities} = props;
+  // let empty = true;
+  const dispatch = useDispatch();
 
-  
+
+  const [inputArray, setInputArray] = useState(""); // aca me viene la data del input (name,diff,dur,season)
+  const [dualColumnArray, setDualColumnArray] = useState([]); // aca me viene la data del scroll (paises)
+  const [random, setRandom] = useState([]);
+  let ran = [];
+  const [randomIsChecked, setRandomIsChecked] = useState(false);
+  const [formSuccess, setFormSuccess] = useState(false);
+
+  let emptyCountries = true;
+  let emptyActivity = true;
+
+
+
+  useEffect(() => {
+    dispatch (getAllCountries());
+
+
+    //console.log(dispatch (getCountryDetailByString("islan")))
+  }, []);
 
   const updateArrayItem = (newValue) => {
     setInputArray([inputArray, newValue ]);
   };
+  
+  const updateArrayCountries = (newValue) => {
+    setDualColumnArray([dualColumnArray, newValue ]);
+  };
+  
+  const handleRandomIsChecked = (event) => {
+    console.log("random es:",  event.target.checked)
+    setRandomIsChecked(event.target.checked);
+    //  if(emptyCountries) setRandomIsChecked(false)
+    // console.log("random :",  randomIsChecked)
+  };
 
-  const ActivityResponseBuilder = (inputArray, dualColumnArray) => {
 
+  const handleRandomChange = (event) => {
+    const value = event.target.value;
+        if (event.target.checked) {
+          setRandom([...random, value]);
+        }
+        else {
+          setRandom(random.filter((e) => e !== value));
+        }
   }
 
+ 
+
+  const ActivityResponseBuilder = (inputArray, dualColumnArray) => {
+    return { 
+              name: inputArray[0],
+              difficulty: inputArray[1],
+              duration: inputArray[2],
+              season: inputArray[3],
+              arrayCountries: dualColumnArray,
+            }
+  }
+
+  function handleReturn() {
+    window.history.back()
+  }
+// console.log("++++++++++++++++++++++++++++++++++++++++++++");
+// consologuero(allCountries)
+// console.log("++++++++++++++++++++++++++++++++++++++++++++");
 
   function consologuero(aux){
     console.log("ESTE ES UN CONSOLELOG: ", aux)
   }
-  consologuero(inputArray[1])
+  // consologuero(inputArray[1]) // aca me viene la data del input (name,diff,dur,season)
+  // consologuero(dualColumnArray[1])
+  // console.log("ESTOS SON LOS PASISES", consologuero(dualColumnArray[1]))
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(dualColumnArray[1][0].length === 0) { emptyCountries=true; }
+    else { emptyCountries=false }
+
+    if(inputArray[1]===undefined) { emptyActivity = true; }
+    else { emptyActivity = false; }
+
+    if(random.length !== 0 && !emptyActivity) {
+      console.log("////////////////RANDOM HABILITADO");
+      console.log(ActivityResponseBuilder(inputArray[1], random))
+      props.createActivity( ActivityResponseBuilder(inputArray[1], random) )
+      handleReturn()
+    }
+    else if(!emptyActivity && !emptyCountries && random.length === 0){
+      console.log(ActivityResponseBuilder(inputArray[1], dualColumnArray[1][0]))
+      props.createActivity (ActivityResponseBuilder(inputArray[1], dualColumnArray[1][0]) )
+      handleReturn()
+    }
+
+  };
+
 
   return (
     
@@ -79,15 +163,27 @@ function ActivityCreator() {
 
           <td>
           {/* <ChildComponent onArrayUpdate={(newArray) => setMyArray(newArray)} /> */}
-            <InputActivityCreator array={inputArray} updateArrayItem={updateArrayItem}  onChange={consologuero}/>
+            <InputActivityCreator array={inputArray} updateArrayItem={updateArrayItem}  />
 
             <div className={styles.row}>
-              <button type="submit">Create Activity</button>
+              <label>
+                  <input type="checkbox" value="Random" checked={random.includes("Random")} onClick={handleRandomChange} onChange={handleRandomIsChecked}
+                  />
+                Use Random's Countries.
+              </label>
             </div>
+
+
+            <div className={styles.row}>
+              <button type="submit" onClick={handleSubmit} disabled={false} >Create Activity</button>
+            </div>
+
+         
+
           </td>
 
-          <td>
-            <DualColumnScrollBarLabel/>
+          <td >
+            <DualColumnScrollBarLabel array={dualColumnArray} updateArrayCountries={updateArrayCountries}  AllCountries={allCountries} disabled={randomIsChecked}/>
           </td>
 
         </tr>
@@ -96,4 +192,23 @@ function ActivityCreator() {
 
 )}
 
-export default ActivityCreator;
+// export default ActivityCreator;
+
+export function mapStateToProps(state) {
+  return {
+    allCountries: state.allCountries,
+    allActivities: state.allActivities,
+
+  };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    getAllCountries: () => dispatch ( getAllCountries() ),
+    getActivities: () => dispatch ( getActivities() ),
+    createActivity: (activity) => dispatch( createActivity(activity) )
+
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActivityCreator);
--- client/src/components/ActivityCreator/DualColumnScrollBarLabel.jsx
+++ client/src/components/ActivityCreator/DualColumnScrollBarLabel.jsx
@@ -1,9 +1,9 @@
 // import React, { useState } from 'react';
-import React, {  useState } from "react";
+import React, {  useState, useEffect } from "react";
 // import ReactDOM from 'react-dom';
 // import { render } from "react-dom";
 import styles from './DualColumnScrollBarLabel.module.css';
-const countryOptions = ["ISL","JPN","NCL","SOM","JOR","BES","DZA","NZL","CHL","FRA","LSO","LBN","TCA","NOR","PER","SYR","NRU","FJI","HND","BEN","PLW","MAF","SJM","MNE","EST","GRD","SGP","MLI","KAZ","ESP","MCO","SYC","ARM","CHN","MYS","ALA","AND","TUR","WLF","ETH","PRY","CZE","IRN","GNB","BRN","SDN","AUT","VEN","SWZ","PSE","STP","MNP","KEN","PNG","TCD","TWN","CPV","VCT","GUY","TKM","MHL","IMN","KWT","TGO","SLE","ZAF","SHN","BVT","MWI","CYM","GBR","ITA","LKA","NFK","AIA","ATF","TUV","CYP","CXR","MUS","SGS","GAB","COD","AUS","FSM","COG","ARG","NLD","LAO","USA","ECU","TON","UNK","LVA","GIN","BIH","BFA","EGY","ZWE","COK","GLP","ABW","BEL","HMD","FRO","GTM","VGB","MNG","ALB","ARE","CRI","KNA","MOZ","PAN","AGO","HUN","PCN","LCA","MTQ","PAK","BLR","SWE","ROU","NAM","WSM","SVK","COL","SRB","JAM","HRV","DJI","FIN","VNM","MKD","PRI","ERI","BGD","LUX","REU","BDI","POL","UGA","PRT","PHL","BTN","LBY","GRL","HKG","UKR","BGR","GMB","NPL","IND","DMA","NER","MDG","UMI","ZMB","RWA","CCK","BLM","CIV","BRA","TUN","VUT","BOL","CAN","AFG","SXM","SLV","SAU","ESH","CAF","TZA","MAC","SEN","OMN","JEY","PYF","GGY","BLZ","NIU","NIC","LIE","TJK","MMR","SUR","MYT","ATG","ASM","GRC","AZE","SLB","BHR","BHS","VAT","MAR","GUM","MSR","DNK","RUS","COM","FLK","MLT","GIB","KOR","URY","TKL","MDV","GHA","MDA","ATA","MEX","KGZ","IOT","QAT","KHM","VIR","CHE","IDN","BWA","IRQ","NGA","LBR","BRB","YEM","IRL","CUW","CUB","SPM","PRK","MRT","UZB","GEO","HTI","THA","GUF","BMU","DOM","ISR","SSD","GNQ","DEU","TLS","TTO","KIR","SVN","SMR","CMR","LTU"];
+// const countryOptions = ["ISL","JPN","NCL","SOM","JOR","BES","DZA","NZL","CHL","FRA","LSO","LBN","TCA","NOR","PER","SYR","NRU","FJI","HND","BEN","PLW","MAF","SJM","MNE","EST","GRD","SGP","MLI","KAZ","ESP","MCO","SYC","ARM","CHN","MYS","ALA","AND","TUR","WLF","ETH","PRY","CZE","IRN","GNB","BRN","SDN","AUT","VEN","SWZ","PSE","STP","MNP","KEN","PNG","TCD","TWN","CPV","VCT","GUY","TKM","MHL","IMN","KWT","TGO","SLE","ZAF","SHN","BVT","MWI","CYM","GBR","ITA","LKA","NFK","AIA","ATF","TUV","CYP","CXR","MUS","SGS","GAB","COD","AUS","FSM","COG","ARG","NLD","LAO","USA","ECU","TON","UNK","LVA","GIN","BIH","BFA","EGY","ZWE","COK","GLP","ABW","BEL","HMD","FRO","GTM","VGB","MNG","ALB","ARE","CRI","KNA","MOZ","PAN","AGO","HUN","PCN","LCA","MTQ","PAK","BLR","SWE","ROU","NAM","WSM","SVK","COL","SRB","JAM","HRV","DJI","FIN","VNM","MKD","PRI","ERI","BGD","LUX","REU","BDI","POL","UGA","PRT","PHL","BTN","LBY","GRL","HKG","UKR","BGR","GMB","NPL","IND","DMA","NER","MDG","UMI","ZMB","RWA","CCK","BLM","CIV","BRA","TUN","VUT","BOL","CAN","AFG","SXM","SLV","SAU","ESH","CAF","TZA","MAC","SEN","OMN","JEY","PYF","GGY","BLZ","NIU","NIC","LIE","TJK","MMR","SUR","MYT","ATG","ASM","GRC","AZE","SLB","BHR","BHS","VAT","MAR","GUM","MSR","DNK","RUS","COM","FLK","MLT","GIB","KOR","URY","TKL","MDV","GHA","MDA","ATA","MEX","KGZ","IOT","QAT","KHM","VIR","CHE","IDN","BWA","IRQ","NGA","LBR","BRB","YEM","IRL","CUW","CUB","SPM","PRK","MRT","UZB","GEO","HTI","THA","GUF","BMU","DOM","ISR","SSD","GNQ","DEU","TLS","TTO","KIR","SVN","SMR","CMR","LTU"];
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
@@ -15,65 +15,89 @@ const myComponent = {
 };
 
 
-export default function DualColumnScrollBarLabel() {
-
+export default function DualColumnScrollBarLabel(props) {
+    const { AllCountries } = props;
     const [selectedCountries, setSelectedCountries] = useState([]);
+    const { disabled } = props;
+
+    
+
+    
     const handleCountryChange = (event) => {
     const value = event.target.value;
-    if (event.target.checked) {
-      setSelectedCountries([...selectedCountries, value]);
-    } else {
-      setSelectedCountries(selectedCountries.filter((country) => country !== value));
-    }
-  };
+        if (event.target.checked) {
+          setSelectedCountries([...selectedCountries, value]);
+          
+        } else {
+          setSelectedCountries(selectedCountries.filter((country) => country !== value));
+        }
 
-  function consologuero(){
-    console.log("ESTE ES UN CONSOLELOG")
+        // aux.push(selectedCountries)
+        // props.updateArrayCountries(aux)
+    };
+
+  useEffect(() => {
+    Response()
+  }, [selectedCountries]);
+
+// console.log("++++++++++++++++++++++++++++++++++++++++++++");
+//   consologuero(selectedCountries)
+//   console.log("++++++++++++++++++++++++++++++++++++++++++++");
+
+  function Response(){
+    let aux = [];
+    aux.push(selectedCountries)
+    props.updateArrayCountries(aux)
   }
 
-
+  function consologuero(aux){
+    console.log("ESTE ES UN CONSOLELOG: ", aux)
+  }
     return (
         <div 
+        // onChange={Response}
         // style={{ height: '200px' }}
         >
-            <div style={myComponent} onChange={consologuero}>
-                <ul>
+            <div style={myComponent} >
+                <ul >
                     
-                <div className={styles['scrollable-container']}>
+                <div className={styles['scrollable-container']} disabled={disabled}>
                     <div className={styles.column}>
-                        {countryOptions.slice(0, Math.ceil(countryOptions.length / 2)).map((country, index) => (
-                        <div key={index}>
+                        {AllCountries.slice(0, Math.ceil(AllCountries.length / 2)).map((country, index) => (
+                        <div key={country.ID}>
                             <label>
                             <input
                                 type="checkbox"
-                                value={country}
-                                checked={selectedCountries.includes(country)}
-                                onChange={handleCountryChange}
+                                value={country.ID}
+                                checked={selectedCountries.includes(country.ID)}
+                                onClick={handleCountryChange}
+                                // onChange={Response}
                             />
-                            {country}
+                            {country.name}
                             </label>
                         </div>
                         ))}
                     </div>
                     <div className={styles.column}>
-                        {countryOptions.slice(Math.ceil(countryOptions.length / 2)).map((country, index) => (
-                        <div key={index}>
+                        {AllCountries.slice(Math.ceil(AllCountries.length / 2)).map((country, index) => (
+                        <div key={country.ID}>
                             <label>
                             <input
                                 type="checkbox"
-                                value={country}
-                                checked={selectedCountries.includes(country)}
-                                onChange={handleCountryChange}
+                                value={country.ID}
+                                checked={selectedCountries.includes(country.ID)}
+                                onClick={handleCountryChange}
+                                // onChange={Response}
                             />
-                            {country}
+                            {country.name}
                             </label>
                         </div>
                          ))}
                     </div>
                 </div>
 
-                </ul>
-            </div>
+                </ul >
+            </div >
         </div>
     );
 };
